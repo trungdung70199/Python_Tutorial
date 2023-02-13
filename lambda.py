a3ff4e3be5d59a63949e37c6fc3747f447510933
trungdung70199 <108038744+trungdung70199@users.noreply.github.com>
2023-02-13 11:05:36 +0900

    rose

---
+++ lambda.py
@@ -0,0 +1,10 @@
+# Lambda
+# A lambda function is a small anonymous function
+# Syntax
+# lambda argument: expression
+x = lambda a: a +10
+print(x(5))
+
+# Lambda function is excuted and the result the result
+x = lambda a, b, c: a + b +c
+print(x(5,2,3))
