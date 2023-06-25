e386617f66fe148ff02222a48fa15faa25270a1f
trungdung70199 <trungdung70199@gmail.com>
2023-06-25 20:53:59 +0900

    Up file

--- Matlotlib_Marker.py
+++ Matlotlib_Marker.py
@@ -13,4 +13,4 @@ import numpy as np
 y = np.array([3, 8, 1, 10])
 plt.plot(y, marker = 'o', ms = 20, mfc = 'r')
 # plt.plot(y, marker = 'o', ms = 20, mec = 'g', mfc = 'r')
-plt.sh 'r'ow()
+plt.show()
---
+++ random_1.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pylot as plt
+
+x = np.random(100)
+y = np.random(100)
+
+plt.scatter(x, y)
+plt.show()
