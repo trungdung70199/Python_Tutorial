f00985883e8aecaf8f6471063ad994d05cefcd29
trungdung70199 <trungdung70199@gmail.com>
2024-06-14 18:11:51 +0900

    add data set

---
+++ DataSet.py
@@ -0,0 +1,16 @@
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy 
+import matplotlib.pyplot as plt
+numpy.random.seed(2)
+
+x = numpy.random.normal(3, 1, 100)
+y = numpy.random.normal(150, 40, 100) / x
+
+plt.scatter(x, y)
+plt.show()
+
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
