8aa0aa528bbafdc2459742e7d061af30f547669e
trungdung70199 <trungdung70199@gmail.com>
2024-11-17 23:53:16 +0900

    add type in python

---
+++ Python/Variable.py
@@ -0,0 +1,25 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable beahaves as if it was the value it contains
+
+# String
+first_name = "D"
+food = "pizza"
+email = "trungdung70199@gmmail.com"
+
+# f-string: to use formatted string literals
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+# Integers
+age = 25
+quantity = 3
+num_of_students = 40
+
+print(f"You are {age} year old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float
+
