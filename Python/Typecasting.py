d0dd7b1de4650d4b7f4ff9bcf748bc47e03533f4
trungdung70199 <trungdung70199@gmail.com>
2024-11-19 05:53:20 +0900

    add type casting

---
+++ Python/Typecasting.py
@@ -0,0 +1,10 @@
+# Typecasting = The process of converting a variable from one data type
+# to another str(), int(), float(), bool()
+
+name = "D"
+age = "25"
+gpa = 3.0
+is_student = True
+
+gpa = int(gpa)
+print(gpa)
