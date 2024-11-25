6130cad050f388aaf65e3fb615eed36b7aa511ed
trungdung70199 <trungdung70199@gmail.com>
2024-11-26 05:27:03 +0900

    add input

---
+++ Python/Input.py
@@ -0,0 +1,8 @@
+# input() = A function that prompts the user to enter data
+# Returns the entered data as a string
+
+name = input("What is your name?: ")
+age = input("How old are you?: ")
+
+print(f"Hello {name}!")
+print(f"You are {age} year old")
