5231d9c19c31c7b8d52c9ad121756f02d922ffc4
trungdung70199 <trungdung70199@gmail.com>
2025-01-29 02:21:43 +0900

    add if

---
+++ If_loop.py
@@ -0,0 +1,13 @@
+# If = Do some code only if some condition is True
+#      Else do something else
+
+age = int(input("Enter your age: "))
+
+if age >= 100:
+    print("You are too old to sign up")
+elif age >= 18:
+    print("You are now signed up!")
+elif age < 0:
+    print("You haven't been born yet!")
+else:
+    print("You must be 18+ to sign up")
