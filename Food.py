2319ec7a733e181daae0c8b1d7180cea129f87d4
trungdung70199 <trungdung70199@gmail.com>
2025-01-29 02:29:57 +0900

    add

---
+++ Food.py
@@ -0,0 +1,6 @@
+response = input("Would you like food? (Y/N): ")
+
+if response == "Y":
+    print("Have some food!")
+else:
+    print("No food for you!")
---
+++ Input_Name.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+
+if name == "":
+    print("You did not type in your name!")
+else:
+    print(f"Hello {name}")
